7269c8772844ca5810c3cd4f27d72aeb61c8be53
yusei-hashimoto <syutard@yahoo.co.jp>
2024-02-25 18:14:52 +0900

    買い物リストと購入済みリストの作成
    リストのリアクティブ化

--- src/views/ShoppingList/ShoppingListView.vue
+++ src/views/ShoppingList/ShoppingListView.vue
@@ -1,24 +1,42 @@
  <template>
-    <h2>買い物リスト</h2>
+    
     <v-container>
+        <h2>買い物リスト</h2>
         <v-data-table
             :headers="headersShoppingListTable"
             :items="shopItemListInit"
-            item-key="name"
+            v-model="shopItemListInit"
+            item-key="id"
             show-select>
             <template #headers></template><!-- v-data-tabeeのヘッダーを削除-->
             <template #bottom></template><!-- v-data-tabeeのフッターを削除-->
+            <template v-slot:item.data-table-select="{ item, isSelected }" >
+                <v-checkbox @change="buyItem(item)">
+                </v-checkbox>
+            </template>
         </v-data-table>
-        <v-checkbox label="Checkbox 1" color="red" value="red"></v-checkbox>
     </v-container>
-    <v-checkbox label="Checkbox"></v-checkbox>
+    <v-container>
+        <h2>購入済みリスト</h2>
+        <v-data-table
+            :headers="headersBuyedItemListTable"
+            :items="buyedItemListInit"
+            v-model="buyedItemListInit"
+        >
+            <template #headers></template><!-- v-data-tabeeのヘッダーを削除-->
+            <template #bottom></template><!-- v-data-tabeeのフッターを削除-->
+        </v-data-table>
+
+    </v-container>
 </template>
 
 <script setup lang="ts">
+    import {reactive} from "vue";
     import type {VDataTable} from 'vuetify/components';
     type VDataTableHeader = VDataTable["headers"];
 
     const headersShoppingListTable: VDataTableHeader = [
+        {key: "id"},
         {key: "name"}
     ];
 
@@ -28,8 +46,29 @@
         name: string
     };
 
-    const shopItemListInit: shopItem[] = [
+    const shopItemListInit: shopItem[] = reactive([
         {id:1, name:"ミックスナッツ"},
         {id:2, name:"うどん"}
+    ]);
+    //購入済みリスト
+    interface buyedItem{
+        id:number,
+        name: string
+    };
+    const headersBuyedItemListTable: VDataTableHeader = [
+        {key: "id"},
+        {key: "name"}
     ];
+    const buyedItemListInit: buyedItem[] = reactive([]);
+    function buyItem(item: buyedItem):void {
+        console.log("購入済みリスト追加イベント");
+        console.log("完了", item);
+        console.log(shopItemListInit.findIndex(element => element.id === item.id));
+        const idShopItem =  shopItemListInit.findIndex(element => element.id === item.id);
+        shopItemListInit.splice(idShopItem, 1);
+        buyedItemListInit.push(item);
+        console.log("購入済みリスト", buyedItemListInit)
+    }
+
+
 </script>
